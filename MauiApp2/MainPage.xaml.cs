53dd126c849f8c964f005e87e5498bb82bed0742
ceima <ceima@S4-02>
2025-08-01 21:08:10 -0300

    Agregar archivos de proyecto.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private async void OnFotoBtnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var foto = await MediaPicker.CapturePhotoAsync();
+                if (foto != null) 
+                {
+                    var stream = await foto.OpenReadAsync();
+                    await DisplayAlert("Sabelo", "Te sacaste la pic bro", "Vpi");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERROR", "La camara no aguanta tanta facha", "Cerrar");
+            }
+        }
+
+        private async void OnVideoBtnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var video = await MediaPicker.CaptureVideoAsync();
+                if (video != null)
+                {
+                    var stream = await video.OpenReadAsync();
+                    await DisplayAlert("Si señor", "Basuuuura", "Sisi");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERROR", "Ojo con lo que filmas sucio", "Cerrar");
+            }
+        }
+    }
+
+}
